8108eadb9805cca9a9e53b67937f3d6e6afa67cc
Jeremy <jeremylimjw@gmail.com>
2021-09-14 01:52:52 +0800

    Minor UI improvements

--- src/app/_db/job.ts
+++ src/app/_db/job.ts
@@ -5,276 +5,276 @@ export const jobs: Job[] = [
         id: 0,
         name: "Adele",
         category: "Warrior",
-        imageUrl: "./assets/job/adele.jpg"
+        imageUrl: "./assets/job/adele.png"
     },
     {
         id: 1,
         name: "Angelic Buster",
         category: "Pirate",
-        imageUrl: "./assets/job/angelic_buster.jpg"
+        imageUrl: "./assets/job/angelic_buster.png"
     },
     {
         id: 2,
         name: "Aran",
         category: "Warrior",
-        imageUrl: "./assets/job/aran.jpg"
+        imageUrl: "./assets/job/aran.png"
     },
     {
         id: 3,
         name: "Ark",
         category: "Pirate",
-        imageUrl: "./assets/job/ark.jpg"
+        imageUrl: "./assets/job/ark.png"
     },
     {
         id: 4,
         name: "Battle Mage",
         category: "Magician",
-        imageUrl: "./assets/job/battle-mage.jpg"
+        imageUrl: "./assets/job/battle_mage.png"
     },
     {
         id: 5,
         name: "Bishop",
         category: "Magician",
-        imageUrl: "./assets/job/magician.jpg"
+        imageUrl: "./assets/job/magician.png"
     },
     {
         id: 6,
         name: "Blaster",
         category: "Warrior",
-        imageUrl: "./assets/job/blaster.jpg"
+        imageUrl: "./assets/job/blaster.png"
     },
     {
         id: 7,
         name: "Blaze Wizard",
         category: "Magician",
-        imageUrl: "./assets/job/blaze_wizard.jpg"
+        imageUrl: "./assets/job/blaze_wizard.png"
     },
     {
         id: 8,
         name: "Bowmaster",
         category: "Bowman",
-        imageUrl: "./assets/job/bowman.jpg"
+        imageUrl: "./assets/job/bowman.png"
     },
     {
         id: 9,
         name: "Buccaneer",
         category: "Pirate",
-        imageUrl: "./assets/job/pirate.jpg"
+        imageUrl: "./assets/job/pirate.png"
     },
     {
         id: 10,
         name: "Cadena",
         category: "Thief",
-        imageUrl: "./assets/job/cadena.jpg"
+        imageUrl: "./assets/job/cadena.png"
     },
     {
         id: 11,
         name: "Cannon Master",
         category: "Pirate",
-        imageUrl: "./assets/job/cannon_master.jpg"
+        imageUrl: "./assets/job/cannon_master.png"
     },
     {
         id: 12,
         name: "Corsair",
         category: "Pirate",
-        imageUrl: "./assets/job/pirate.jpg"
+        imageUrl: "./assets/job/pirate.png"
     },
     {
         id: 13,
         name: "Crossbow Master",
         category: "Bowman",
-        imageUrl: "./assets/job/bowman.jpg"
+        imageUrl: "./assets/job/bowman.png"
     },
     {
         id: 14,
         name: "Dark Knight",
         category: "Warrior",
-        imageUrl: "./assets/job/warrior.jpg"
+        imageUrl: "./assets/job/warrior.png"
     },
     {
         id: 15,
         name: "Demon Avenger",
         category: "Warrior",
-        imageUrl: "./assets/job/demon_avenger.jpg"
+        imageUrl: "./assets/job/demon_avenger.png"
     },
     {
         id: 16,
         name: "Demon Slayer",
         category: "Warrior",
-        imageUrl: "./assets/job/demon_slayer.jpg"
+        imageUrl: "./assets/job/demon_slayer.png"
     },
     {
         id: 17,
         name: "Dual Blade",
         category: "Thief",
-        imageUrl: "./assets/job/dual_blade.jpg"
+        imageUrl: "./assets/job/dual_blade.png"
     },
     {
         id: 18,
         name: "Eunwol",
         category: "Pirate",
-        imageUrl: "./assets/job/eunwol.jpg"
+        imageUrl: "./assets/job/eunwol.png"
     },
     {
         id: 19,
         name: "Evan",
         category: "Magician",
-        imageUrl: "./assets/job/evan.jpg"
+        imageUrl: "./assets/job/evan.png"
     },
     {
         id: 20,
         name: "Fire/Poison Mage",
         category: "Magician",
-        imageUrl: "./assets/job/magician.jpg"
+        imageUrl: "./assets/job/magician.png"
     },
     {
         id: 21,
         name: "Hayato",
         category: "Warrior",
-        imageUrl: "./assets/job/hayato.jpg"
+        imageUrl: "./assets/job/hayato.png"
     },
     {
         id: 22,
         name: "Hero",
         category: "Warrior",
-        imageUrl: "./assets/job/warrior.jpg"
+        imageUrl: "./assets/job/warrior.png"
     },
     {
         id: 23,
         name: "Hoyoung",
         category: "Thief",
-        imageUrl: "./assets/job/hoyoung.jpg"
+        imageUrl: "./assets/job/hoyoung.png"
     },
     {
         id: 24,
         name: "Illium",
         category: "Magician",
-        imageUrl: "./assets/job/illium.jpg"
+        imageUrl: "./assets/job/illium.png"
     },
     {
         id: 25,
         name: "Ice/Lightning Mage",
         category: "Magician",
-        imageUrl: "./assets/job/magician.jpg"
+        imageUrl: "./assets/job/magician.png"
     },
     {
         id: 26,
         name: "Kaine",
         category: "Bowman",
-        imageUrl: "./assets/job/kaine.jpg"
+        imageUrl: "./assets/job/kaine.png"
     },
     {
         id: 27,
         name: "Kaiser",
         category: "Warrior",
-        imageUrl: "./assets/job/kaiser.jpg"
+        imageUrl: "./assets/job/kaiser.png"
     },
     {
         id: 28,
         name: "Kanna",
         category: "Magician",
-        imageUrl: "./assets/job/kanna.jpg"
+        imageUrl: "./assets/job/kanna.png"
     },
     {
         id: 29,
         name: "Kinesis",
         category: "Magician",
-        imageUrl: "./assets/job/kinesis.jpg"
+        imageUrl: "./assets/job/kinesis.png"
     },
     {
         id: 30,
         name: "Luminous",
         category: "Magician",
-        imageUrl: "./assets/job/Luminous.jpg"
+        imageUrl: "./assets/job/Luminous.png"
     },
     {
         id: 31,
         name: "Mechanic",
         category: "Pirate",
-        imageUrl: "./assets/job/mechanic.jpg"
+        imageUrl: "./assets/job/mechanic.png"
     },
     {
         id: 32,
         name: "Mercedes",
         category: "Bowman",
-        imageUrl: "./assets/job/mercedes.jpg"
+        imageUrl: "./assets/job/mercedes.png"
     },
     {
         id: 33,
         name: "Mihile",
         category: "Warrior",
-        imageUrl: "./assets/job/mihile.jpg"
+        imageUrl: "./assets/job/mihile.png"
     },
     {
         id: 34,
         name: "Night Lord",
         category: "Thief",
-        imageUrl: "./assets/job/phantom.jpg"
+        imageUrl: "./assets/job/phantom.png"
     },
     {
         id: 35,
         name: "Night Walker",
         category: "Thief",
-        imageUrl: "./assets/job/night_walker.jpg"
+        imageUrl: "./assets/job/night_walker.png"
     },
     {
         id: 36,
         name: "Pathfinder",
         category: "Bowman",
-        imageUrl: "./assets/job/pathfinder.jpg"
+        imageUrl: "./assets/job/pathfinder.png"
     },
     {
         id: 37,
         name: "Paladin",
         category: "Warrior",
-        imageUrl: "./assets/job/warrior.jpg"
+        imageUrl: "./assets/job/warrior.png"
     },
     {
         id: 38,
         name: "Shadower",
         category: "Thief",
-        imageUrl: "./assets/job/phantom.jpg"
+        imageUrl: "./assets/job/phantom.png"
     },
     {
         id: 39,
         name: "Soul Master",
         category: "Warrior",
-        imageUrl: "./assets/job/soul_master.jpg"
+        imageUrl: "./assets/job/soul_master.png"
     },
     {
         id: 40,
         name: "Striker",
         category: "Pirate",
-        imageUrl: "./assets/job/striker.jpg"
+        imageUrl: "./assets/job/striker.png"
     },
     {
         id: 41,
         name: "Thief",
         category: "Thief",
-        imageUrl: "./assets/job/thief.jpg"
+        imageUrl: "./assets/job/thief.png"
     },
     {
         id: 42,
         name: "Wild Hunter",
         category: "Bowman",
-        imageUrl: "./assets/job/wild_hunter.jpg"
+        imageUrl: "./assets/job/wild_hunter.png"
     },
     {
         id: 43,
         name: "Wind Breaker",
         category: "Bowman",
-        imageUrl: "./assets/job/wind_breaker.jpg"
+        imageUrl: "./assets/job/wind_breaker.png"
     },
     {
         id: 44,
         name: "Xenon",
         category: "Pirate",
-        imageUrl: "./assets/job/xenon.jpg"
+        imageUrl: "./assets/job/xenon.png"
     },
     {
         id: 45,
         name: "Zero",
         category: "Warrior",
-        imageUrl: "./assets/job/zero.jpg"
+        imageUrl: "./assets/job/zero.png"
     },
 ]
